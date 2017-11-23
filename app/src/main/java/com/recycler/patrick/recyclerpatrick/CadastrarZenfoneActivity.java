0b61981ab4faad5078c8466637bb99d854049f16
Patrick <trick_stival@hotmail.com>
2017-11-23 03:00:44 -0200

    Adicionado botão de criar zenfone

--- app/src/main/java/com/recycler/patrick/recyclerpatrick/CadastrarZenfoneActivity.java
+++ app/src/main/java/com/recycler/patrick/recyclerpatrick/CadastrarZenfoneActivity.java
@@ -2,12 +2,31 @@ package com.recycler.patrick.recyclerpatrick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CadastrarZenfoneActivity extends AppCompatActivity {
+
+    private EditText txtNomeZ,
+                     txtAnoZ;
+    private Spinner spnModelo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_zenfone);
+
+        initComponents();
     }
+
+    private void initComponents(){
+        txtNomeZ = (EditText) findViewById(R.id.txtNomeZenfone);
+        txtAnoZ = (EditText) findViewById(R.id.txtAnoZenfone);
+    }
+
+    public void adicionarZenfone(View btnAdd){
+
+    }
+
 }
